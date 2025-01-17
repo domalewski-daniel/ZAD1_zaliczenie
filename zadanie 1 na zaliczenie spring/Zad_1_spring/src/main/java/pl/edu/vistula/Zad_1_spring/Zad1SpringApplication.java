a795785d8f346cb6153666ab4953c6ebcf3b1029
Daniel <domalewski.d.daniel@gmail.com>
2025-01-17 11:24:48 +0100

    Add main to git

---
+++ zadanie 1 na zaliczenie spring/Zad_1_spring/src/main/java/pl/edu/vistula/Zad_1_spring/Zad1SpringApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.vistula.Zad_1_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Zad1SpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Zad1SpringApplication.class, args);
+	}
+
+}
